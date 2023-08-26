33eb49d6fd5fd9f473d8ad1eba4cbcbb75f06df4
GuilhermeLCS <guilhermeluiscdasilva@hotmail.com>
2023-08-26 02:15:49 -0300

    Página de login criada

--- src/components/forms/LoginForm.jsx
+++ src/components/forms/LoginForm.jsx
@@ -1,21 +1,51 @@
-import React from 'react';
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
+import React, { useState } from 'react';
+import { Container, Paper, TextField, Button, Typography } from '@mui/material';
 
-export function LoginForm() {
-    return (
-      <div>
-        <Box
-          component="form"
-          sx={{
-              '& > :not(style)': { m: 1, width: '25ch' },
-          }}
-          noValidate
-          autoComplete="off"
-          >
-          <TextField id="outlined-basic" label="E-mail" variant="outlined" />
-          <TextField id="outlined-basic" label="Senha" variant="outlined" />
-        </Box>
-      </div>
-    );
-  }
+function LoginForm() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleLogin = () => {
+    // Lógica de autenticação aqui
+  };
+
+  return (
+    <Container component={Paper} elevation={3} maxWidth="xs" sx={{ padding: 3 }}>
+      <Typography variant="h5" sx={{ textAlign: 'center' }}>
+        Entrar
+      </Typography>
+      <TextField
+        label="E-mail"
+        fullWidth
+        value={email}
+        onChange={handleEmailChange}
+        margin="normal"
+      />
+      <TextField
+        label="Senha"
+        type="password"
+        fullWidth
+        value={password}
+        onChange={handlePasswordChange}
+        margin="normal"
+      />
+      <Button variant="contained" color="primary" fullWidth onClick={handleLogin}>
+        Entrar
+      </Button>
+      <p></p>
+      <Typography variant="p" sx={{ textAlign: 'center' }}>
+        Ainda não tem uma conta? <a href="/">Clique aqui.</a> 
+      </Typography>
+    </Container>
+  );
+}
+
+export default LoginForm;
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -3,21 +3,14 @@ import React from 'react';
 
 
 import './Login.css';
-import SimpleContainer from '../../components/containers/ContainerSM';
-import { LoginForm } from '../../components/forms/LoginForm';
+import { ContainerPos } from '../../components/containers/ContainerPos';
+import LoginForm from '../../components/forms/LoginForm';
 
 export default function Login() {
   return (
-    <div>
-      <SimpleContainer>
-        <LoginForm></LoginForm>
-        <h1>Olá!</h1>
-      </SimpleContainer>
-      <h1>Olá!</h1>
-      <LoginForm></LoginForm>
-      
-    </div>
-    
-    
+    <div className='container-sm-pos'>    
+      <LoginForm></LoginForm>    
+    </div>      
+         
   );
 }
